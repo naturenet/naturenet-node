79e9c3d021efe22876ee472ccc693a20a2577a58
Jason Maher <jason@jasonmaher.me>
2016-01-15 19:49:59 +0900

    spring-boot project template

---
+++ src/main/java/org/naturenet/ApiMockupApplication.java
@@ -0,0 +1,12 @@
+package org.naturenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMockupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMockupApplication.class, args);
+	}
+}
