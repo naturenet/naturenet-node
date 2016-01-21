4c4ff877f96a11343d4dfab3d7bc1ea1e0d66371
Jason Maher <jason@jasonmaher.me>
2016-01-21 12:23:33 +0900

    convert to nodejs to match up with the prod app

---
+++ app.js
@@ -0,0 +1,18 @@
+var _ = require('lodash');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var auth = require('./lib/auth');
+app.use(auth.basic);
+
+app.use('/users', require('./lib/routes/user'));
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+    console.log("NatureNet up and running on port %s", app.get('port'));
+});
