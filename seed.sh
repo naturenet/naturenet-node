06c1378094774b69366df9410e956502d699f9da
Jason Maher <jason@jasonmaher.me>
2016-02-18 09:50:07 +0900

    cleanup the local development scripts so that FIREBASE_URL is always explicitly set

--- seed.sh
+++ seed.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 #
-# Runs the database seed script with the '.env' variables loaded.
+# Runs the database seed script against the staging and test
+# databases
 while read LINE; do
     export "$LINE"
 done < .env
+
+export FIREBASE_URL="$TEST_URL"
+export FIREBASE_SECRET="$TEST_SECRET"
+
+node lib/data/seed-firebase.js
+
+export FIREBASE_URL="$STAGING_URL"
+export FIREBASE_SECRET="$STAGING_SECRET"
 
 node lib/data/seed-firebase.js
